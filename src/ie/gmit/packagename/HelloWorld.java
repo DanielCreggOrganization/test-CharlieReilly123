4b4ed80189c1c761d052535a0c8686b9b0658652
CharlieReilly123 <G00369506@gmit.ie>
2020-01-20 17:28:58 +0000

    added for loop

--- src/ie/gmit/packagename/HelloWorld.java
+++ src/ie/gmit/packagename/HelloWorld.java
@@ -5,6 +5,11 @@ class HelloWorld {
     public static void main(String[] args) {
         // Print Hello World! to the console.
         System.out.println("Hello World!");
+        System.out.println("I exist");
+
+        for(int i=1;i<=10;i++) {
+    System.out.println("Charlie");
+}
     }
 
 }
